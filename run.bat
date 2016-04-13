43ccd782138f4432a101869db505725b5b0f646f
Artwork PC Tonsley <tons0003@flinders.edu.au>
2016-04-13 15:40:16 +0930

    te

--- run.bat
+++ run.bat
@@ -1,9 +1,10 @@
 @echo off
-set DBNAME=leds
+set DBNAME=led
 set DBHOST=localhost
-set DBPASS=
-set DBUSER=
+set DBPASS=ledmanager
+set DBUSER=LEDManager
+
 @echo on
-start python main.py
+start python main.py 172.22.11.2:7890
 cd webapp\TonsleyLED\tonsleyled
 start pserve production.ini --reload
